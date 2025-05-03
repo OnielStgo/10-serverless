1d750ab4eb26f99b8e6417869fd255c71de65448
OnielStgo <onielst84@gmail.com>
2025-05-03 09:39:37 -0300

    edge function para netlify criada

--- netlify/functions/github-discord/github-discord.ts
+++ netlify/functions/github-discord/github-discord.ts
@@ -22,9 +22,65 @@ const notify = async (message: string) => {
 }
 
 
+const onStar = (payload: any): string => {
+
+  const { action, sender, repository, starred_at } = payload
+
+  return `User ${sender.login} ${action} star on ${repository.full_name}`
+}
+
+const onIssue = (payload: any): string => {
+
+  const { action, issue } = payload
+
+  if (action === 'opened') {
+    return `An issue was opened with title ${issue.title}`
+  }
+
+  if (action === 'closed') {
+    return `An issue was closed by ${issue.user.login}`
+  }
+
+  if (action === 'reopened') {
+    return `An issue was reopened by ${issue.user.login}`
+  }
+
+  return `Unhandled action for the issue event ${action}`
+}
+
+
+
 export default async (request: Request, context: Context) => {
 
-  await notify('Olá mundo desde netlify dev')
+  const githubEvent = request.headers.get('x-github-event') ?? 'unknown';
+  // const githubEvent = await request.headers['x-github-event'] ?? 'unknown'
+  const text = await request.text();
+  const payload = JSON.parse(text ?? '{}')
+  let message: string
+
+
+  console.log(`O githubEvent foi: ${githubEvent}`)
+  console.log(`O sender foi: ${payload.sender.login}`)
+  console.log(`A action foi: ${payload.action}`)
+  console.log(`O repositorio é: ${payload.repository.full_name}`)
+
+  // console.log(payload)
+
+  switch (githubEvent) {
+    case 'star':
+      message = onStar(payload)        
+      break;
+
+    case 'issues':
+      message = onIssue(payload)        
+      break;
+  
+    default:
+      message = `Unknown event ${githubEvent}`
+      break;
+  }
+
+  await notify(message)
 
   return new Response(JSON.stringify({message: 'done'}), {
     status: 200,
